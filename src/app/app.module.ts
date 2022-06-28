e250451768dc2975126580d0c338d37d31dee3f9
AlexLzn03 <alexsanderlozano175474@gmail.com>
2022-06-28 15:50:10 -0300

    ProjetoDogs

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DogsComponent } from './dogs/dogs.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DogsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
